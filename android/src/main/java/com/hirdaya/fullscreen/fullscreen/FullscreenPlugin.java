ea1036ffa2193938cd4e5fe326b2688a6664e032
HirdayaShrestha <hirdaya098@gmail.com>
2022-10-21 16:04:42 +0545

    Fullscreen Android Completed

--- android/src/main/java/com/hirdaya/fullscreen/fullscreen/FullscreenPlugin.java
+++ android/src/main/java/com/hirdaya/fullscreen/fullscreen/FullscreenPlugin.java
@@ -75,6 +75,8 @@ public class FullscreenPlugin implements FlutterPlugin, MethodChannel.MethodCall
         WindowInsetsControllerCompat wICC = new WindowInsetsControllerCompat(mainActivity.getWindow(), mainActivity.getWindow().getDecorView());
         wICC.hide(WindowInsetsCompat.Type.systemBars());
         wICC.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            mainActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
             mainActivity.getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
     }
